446fc70c73567437792496bafffb2f7b9304a2d2
naabiiill <nabilmahmud.tech@gmail.com>
2026-07-06 21:24:51 +0600

    learning string methods

---
+++ string_theory.py
@@ -0,0 +1,19 @@
+string_var= "this is a variable string"
+
+additional_string = " and this is an additional string"
+
+output_string = string_var + additional_string
+
+print(output_string)
+
+div_var = 100/3 #33.333333333333336
+
+print("if we divide 100 by 3 we get: ", div_var)
+
+print(f"if we divide 100 by 3 we get: {div_var:.2f}") #33.33
+
+#all cap
+
+name = "john doe"
+print(name.upper()) #JOHN DOE
+print(name.lower()) #john doe
