1d87b1c0a43237283cd9bc72bc8c33eed7e3f969
naabiiill <nabilmahmud.tech@gmail.com>
2026-07-14 23:58:03 +0600

    list-(add,insert,remove,copy,zip)

---
+++ _tuple_dict_set_methods.py
@@ -0,0 +1,8 @@
+# List - Group of Item
+# Add, Insert, remove, copy, access
+#Zip
+names = ["Joy","Apurba","Akash","Nusfa"]
+salaries = [10,1000,10000]
+
+for name,salary in zip(names,salaries):
+  print(f"Name:{name} and salary:{salary}")
