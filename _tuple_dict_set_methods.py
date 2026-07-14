901d952aa485d41ee944f69474bde3a5a297f2bc
naabiiill <nabilmahmud.tech@gmail.com>
2026-07-15 00:01:08 +0600

    using tuple

--- _tuple_dict_set_methods.py
+++ _tuple_dict_set_methods.py
@@ -6,3 +6,21 @@ salaries = [10,1000,10000]
 
 for name,salary in zip(names,salaries):
   print(f"Name:{name} and salary:{salary}")
+
+
+
+#Enumrates
+for index,name in enumerate(names,start=1):
+  print(f"{index}.{name}")
+
+
+# Tuple
+# Feature: Ordered, Unchangeable, Allow Duplicate Value
+marks = (
+    ("Joy",33,"Class 10"),#0
+    ("Erfan",90,"Class 11"),#1
+    ("Apurba",100,"Class 4"),#-2
+    ("Joy",33,"Class 10")#-1
+)
+
+print(marks)  
