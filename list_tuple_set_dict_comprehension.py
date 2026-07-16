2a0c2fe88c7f50126af7ab69841d0c8bec899546
naabiiill <nabilmahmud.tech@gmail.com>
2026-07-16 21:29:14 +0600

    packing and unpacking

--- list_tuple_set_dict_comprehension.py
+++ list_tuple_set_dict_comprehension.py
@@ -12,3 +12,22 @@ print ("\n........\n")
 
 x=[i for i in data_list if i%2==0]
 print(x)
+
+print ("\n.... tuples packing nd unpacking....\n")
+
+
+# 1. tuples packing nd unpacking
+
+dummy_tuple = (1, 2, 3, 4, 5)
+print(type(dummy_tuple))
+
+list_tuple = list(dummy_tuple)  #(unpacking) converting tuple to list
+
+print(type(list_tuple))
+
+list_tuple.append(6) #adding new element to list
+
+dummy_tuple = tuple(list_tuple) #converting list to tuple
+print(type(dummy_tuple))
+
+print(dummy_tuple)
