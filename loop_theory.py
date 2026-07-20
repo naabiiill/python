3ab778a5c0e4e6d41c543053fbb0d2d118721a8f
naabiiill <nabilmahmud.tech@gmail.com>
2026-07-20 23:58:14 +0600

    looping logics

--- loop_theory.py
+++ loop_theory.py
@@ -34,7 +34,7 @@ for i in range (range_val):
     else:
        continue #break 
 
-"""
+
 
 #while loop
 #use when you do not know the number of iterations
@@ -50,3 +50,41 @@ while n>0:
     n -= 1 # n = n-1
 print("The list you created is: ", test_list)
 
+
+"""
+#Loop
+# For and While
+# While know which condition is true and I don't how many iterations we will need to execute a task
+#
+
+#x = range(5)
+#print(x)
+'''
+for i in range(5):
+  print(i)
+
+data_set = {"A","B","C"}
+
+for item in data_set:
+  print(item)
+
+
+for i in range(4,10,2):
+  print(i)
+
+
+for i in range(1,100,5):
+  if i%5 ==0: #1,6,11,16,21,26
+    print(i)
+  elif i%13 == 0:
+    print(i)
+    print("\n end the loop")
+    break
+'''
+#While
+
+x = 6
+while x>5 and x<=100:
+  if x % 6 == 0:
+    print(x)
+  x += 1
